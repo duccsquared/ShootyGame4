04c455250f9ca267cca229d37b8752f36b1924c4
Mario Susanto <msus0004@student.monash.edu>
2023-01-15 23:23:04 +0700

    as reverting didn't work, removed grid related code manually

--- src/game/gameScreen/sprites/GameObject.java
+++ src/game/gameScreen/sprites/GameObject.java
@@ -3,15 +3,9 @@ package game.gameScreen.sprites;
 import engine.objects.BaseObject;
 import engine.objects.Sprite;
 import engine.screens.Screen;
-import game.gameScreen.grids.RegionGrid;
-import game.gameScreen.grids.Tile;
-import game.gameScreen.grids.WorldGrid;
 import game.screens.GameScreen;
 
 public abstract class GameObject extends Sprite {
-    private RegionGrid currentRegion;
-    private Tile currentTile;
-    private WorldGrid worldGrid;
     private boolean isStatic = true;
     public GameObject(Screen screen, int r, int g, int b, int borderR, int borderG, int borderB, double x1, double y1, double x2, double y2) {
         super(screen, r, g, b, borderR, borderG, borderB, x1, y1, x2, y2);
@@ -24,16 +18,6 @@ public abstract class GameObject extends Sprite {
     }
 
     protected void initGameObject() {
-        worldGrid = ((GameScreen) this.getScreen()).getWorldGrid();
-        currentRegion = worldGrid.regionFromCoords(x(),y());
-        if(currentRegion==null) {
-            worldGrid.instantiateRegionGridFromCoords(x(),y());
-            currentRegion = worldGrid.regionFromCoords(x(),y());
-            worldGrid.paint();
-        }
-        currentRegion.addObject(this);
-        currentTile = worldGrid.tileFromCoords(x(),y());
-        currentTile.addObject(this);
     }
 
     @Override
@@ -59,37 +43,10 @@ public abstract class GameObject extends Sprite {
     @Override
     public void delete() {
         super.delete();
-        currentRegion.removeObject(this);
-        currentTile.removeObject(this);
-        currentRegion = null;
-        currentTile = null;
     }
 
     @Override
     public void tick() {
         super.tick();
-        if(!this.isStatic) {
-            RegionGrid nextRegion = currentRegion.getWorldGrid().regionFromCoords(x(),y());
-            if(nextRegion==null) {
-                this.onOutOfBounds();
-            }
-            if(!currentRegion.equals(nextRegion)) {
-                currentRegion.removeObject(this);
-                currentRegion = nextRegion;
-                currentRegion.addObject(this);
-            }
-            Tile nextTile = currentRegion.getWorldGrid().tileFromCoords(x(),y());
-            if(!currentTile.equals(nextTile)) {
-                currentTile.removeObject(this);
-                currentTile = nextTile;
-                currentTile.addObject(this);
-            }
-        }
     }
-    protected void onOutOfBounds() {
-        this.delete();
-    }
-
-    public RegionGrid getCurrentRegion() {return currentRegion;}
-    public Tile getCurrentTile() {return currentTile;}
 }
