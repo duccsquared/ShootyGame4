3cd850f60495105abe693307d5957a13b526aeb7
Mario Susanto <msus0004@student.monash.edu>
2023-01-21 18:07:12 +0700

    redesigned map

--- src/game/screens/GameScreen.java
+++ src/game/screens/GameScreen.java
@@ -13,43 +13,60 @@ public class GameScreen extends BaseScreen {
     Player player;
     public GameScreen() {
         super(id);
-        player = new Player(this,300,300);
-        for(int i = 0; i < 20; i++) {
-            int posX = Global.randInt(-4,24);
-            int posY = Global.randInt(-4,24);
-            new Wall(this,Wall.wallCoordToCoord(posX),Wall.wallCoordToCoord(posY));
-        }
+        player = new Player(this,-600,-600);
+
         new Stabber(this,200,200);
+        this.getCamera().setX(-920);
+        this.getCamera().setY(-920);
         this.getCamera().attachObject(player);
         Faction.setAsHostile(Global.playerFaction,Global.enemyFaction);
-//        for(int x = -4; x <= 24; x++) {
-//            new Wall(this,Wall.wallCoordToCoord(x),Wall.wallCoordToCoord(-4));
-//            new Wall(this,Wall.wallCoordToCoord(x),Wall.wallCoordToCoord(24));
-//        }
-        new Wall(this,
-                Wall.wallCoordToCoord(-24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(-24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(-24)+Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)+Wall.HALF_SIZE);
-        new Wall(this,
-                Wall.wallCoordToCoord(24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(-24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)+Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)+Wall.HALF_SIZE);
-        new Wall(this,
-                Wall.wallCoordToCoord(-24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(-24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)+Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(-24)+Wall.HALF_SIZE);
-        new Wall(this,
-                Wall.wallCoordToCoord(-24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)-Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)+Wall.HALF_SIZE,
-                Wall.wallCoordToCoord(24)+Wall.HALF_SIZE);
-//        for(int y = -4; y <= 24; y++) {
-//            new Wall(this,Wall.wallCoordToCoord(-4),Wall.wallCoordToCoord(y));
-//            new Wall(this,Wall.wallCoordToCoord(24),Wall.wallCoordToCoord(y));
-//        }
+
+        // map borders
+        final int MIN_X = -800;
+        final int MIN_Y = -800;
+        final int MAX_X = 1200;
+        final int MAX_Y = 800;
+        final int MARGIN = 400;
+
+        new Wall(this, MIN_X-MARGIN, MIN_Y, MIN_X, MAX_Y);
+        new Wall(this, MAX_X, MIN_Y, MAX_X+MARGIN, MAX_Y);
+        new Wall(this, MIN_X-MARGIN, MIN_Y-MARGIN, MAX_X+MARGIN, MIN_Y);
+        new Wall(this, MIN_X-MARGIN, MAX_Y, MAX_X+MARGIN, MAX_Y+MARGIN);
+
+        // starting area borders
+        new Wall(this, -400, -800, -368, -400);
+        new Wall(this, -800, -400, -620, -368);
+        new Wall(this, -580, -400, -368, -368);
+
+        // area 1
+        for(int i = 0; i < 40; i++) {
+            int posX = Global.randInt(-350,0);
+            int posY = Global.randInt(-768,768);
+            new Wall(this,posX,posY);
+        }
+        // area 2
+        for(int i = 0; i < 20; i++) {
+            int posX = Global.randInt(0,600);
+            int posY = Global.randInt(-768,768);
+            int width = Global.randInt(32,96);
+            int height = Global.randInt(32,96);
+            new Wall(this,posX,posY,posX+width,posY+height);
+        }
+        // area 3
+        for(int i = 0; i < 20; i++) {
+            int posX = Global.randInt(600,1180);
+            int posY = Global.randInt(-768,768);
+            int width; int height;
+            if(Global.randInt(0,1)==1) {
+                width = Global.randInt(64,128);
+                height = Global.randInt(16,48);
+            }
+            else {
+                width = Global.randInt(16,48);
+                height = Global.randInt(64,128);
+            }
+            new Wall(this,posX,posY,posX+width,posY+height);
+        }
     }
 
 
